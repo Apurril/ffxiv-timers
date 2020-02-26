e517465562092df27698e90ea913d5fa92e9c463
April <April@Rain>
2020-02-26 17:39:44 +0000

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,122 @@ const resources = [
       "Beryllium Ore",
       "Prismstone",
     ],
+    job: "Miner",
     location: {
       zone: "Il Mheg",
       teleport: "Lydha Lran",
       pos: [30, 21],
     },
     times: [4, 16],
+  },
+  {
+    resource: [
+      "Duskblooms",
+    ],
+    job: "Gatherer",
+    location: {
+      zone: "Amh Araeng",
+      teleport: "The Inn at Journey's Head",
+      pos: [32, 33],
+    },
+    times: [4, 16],
+  },
+  {
+    resource: [
+      "Purpure Shell Chip",
+    ],
+    job: "Miner",
+    location: {
+      zone: "The Tempest",
+      teleport: "The Ondo Cups",
+      pos: [34, 31],
+    },
+    times: [6, 18],
+  },
+  {
+    resource: [
+      "Ethereal Cocoon",
+    ],
+    job: "Gatherer",
+    location: {
+      zone: "Lakeland",
+      teleport: "Fort Jobb",
+      pos: [26, 11],
+    },
+    times: [8, 20],
+  },
+  {
+    resource: [
+      "Merbau Log",
+    ],
+    job: "Gatherer",
+    location: {
+      zone: "Il Mheg",
+      teleport: "Wolekdorf",
+      pos: [37, 27],
+    },
+    times: [8, 20],
+  },
+  {
+    resource: [
+      "Tungsten Ore",
+    ],
+    job: "Miner",
+    location: {
+      zone: "The Tempest",
+      teleport: "The Ondo Cups",
+      pos: [33, 8],
+    },
+    times: [10, 22],
+  },
+  {
+    resource: [
+      "Ashen Alumen",
+    ],
+    job: "Miner",
+    location: {
+      zone: "Amh Araeng",
+      teleport: "Mord Souq",
+      pos: [20, 9],
+    },
+    times: [10, 22],
+  },
+  {
+    resource: [
+      "Sandalwood Log",
+      "Sandalwood Sap",
+    ],
+    job: "Gatherer",
+    location: {
+      zone: "The Rak'tika Greatwood",
+      teleport: "Slitherbough",
+      pos: [24, 36],
+    },
+    times: [2, 14],
+  },
+  {
+    resource: [
+      "Fireheart Cobalt",
+    ],
+    job: "Miner",
+    location: {
+      zone: "Lakeland",
+      teleport: "The Ostall Imperative",
+      pos: [5, 34],
+    },
+    times: [2, 14],
+  },
+  {
+    resource: [
+      "Imperial Fern",
+    ],
+    job: "Gatherer",
+    location: {
+      zone: "Kholusia",
+      teleport: "Stilltide",
+      pos: [28, 21],
+    },
+    times: [0, 12],
   },
 ];
 
@@ -34,7 +144,7 @@ const App = () => (
 
 const Card = () => (
   <div className="card">
-    <div>Beryllium Ore</div>
+    <div className="resource-name">Beryllium Ore</div>
     <div>Prismstone</div>
     <div>Il Mheg</div>
     <div>Lydha Lran</div>
